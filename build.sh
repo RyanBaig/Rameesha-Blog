61b785ed95f9e24976bc4ed0a5247a634444b469
RyanBaig <rg5373429@gmail.com>
2024-03-10 13:54:17 +0500

    remove useless build step

--- build.sh
+++ build.sh
@@ -7,7 +7,3 @@ python3.9 -m pip install -r requirements.txt
 echo "Making Migrations..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-
-# Collect static files
-echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput --clear
