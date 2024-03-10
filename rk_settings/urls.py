58fa8f43a03cbb631758412799d87001f139e070
RyanBaig <rg5373429@gmail.com>
2024-03-10 13:42:16 +0500

    hello world!

--- rk_settings/urls.py
+++ rk_settings/urls.py
@@ -23,4 +23,4 @@ from django.urls import include, path
 urlpatterns = [
     path("", include("blog.urls")),
     path("admin/", admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
