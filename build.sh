b94b6d230e04b46c78085bfae9eabf06fcb1b77f
RyanBaig <rg5373429@gmail.com>
2024-03-10 13:19:27 +0500

    eheheheheh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build the project
+echo "Building project..."
+python3.9 -m pip install -r requirements.txt
+
+echo "Making Migrations"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python3.9 manage.py collectstatic --noinput --clear
--- rk_settings/settings.py
+++ rk_settings/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dotenv
+import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-aj!t17lux_*tdqu3+_^hwer+o3v-5)@3o9rxgg&ua1#(q)pa*t"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,9 +86,11 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+
     }
 }
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
